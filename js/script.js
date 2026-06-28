f96a1efe43cb4682a10d76c3fcf3b3bbcb0667ed
Russell <russellmuiga395@gmail.com>
2026-06-28 17:27:24 +0300

    Add Bootstrap tooltips and UI hover animations

--- js/script.js
+++ js/script.js
@@ -164,3 +164,13 @@ if (searchInput) {
     });
 
 }
+
+// Bootstrap Tooltips
+
+const tooltipTriggerList = document.querySelectorAll(
+    '[data-bs-toggle="tooltip"]'
+);
+
+const tooltipList = [...tooltipTriggerList].map(
+    tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl)
+);
